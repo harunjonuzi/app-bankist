af3fcd8000224dfafdd724d5afff8d63538333e9
Harun-Mac <iamharunjonuzi@gmail.com>
2023-09-11 09:46:34 +0200

    refactor: v2

--- app/js/main.js
+++ app/js/main.js
@@ -1,110 +1,7 @@
 "use strict";
+import { account1 } from "./data.js";
 
-// import {
-//   account1,
-//   account2,
-//   account3,
-//   account4,
-//   account5,
-//   account6,
-//   accounts,
-// } from "./data.js";
-
-const account1 = {
-  owner: "Harun Jonuzi",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-
-  movementsDates: [
-    "2022-11-18T21:31:17.178Z",
-    "2022-12-23T07:42:02.383Z",
-    "2023-01-28T09:15:04.904Z",
-    "2023-04-01T10:17:24.185Z",
-    "2023-05-11T14:11:59.604Z",
-    "2023-05-12T17:01:17.194Z",
-    "2023-05-13T20:36:17.929Z",
-    "2023-05-14T10:51:36.790Z",
-  ],
-  currency: "MKD",
-  locale: "mk-MK", // de-DE
-};
-
-const account2 = {
-  owner: "Rita Jonuzi",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-
-  movementsDates: [
-    "2022-11-18T21:31:17.178Z",
-    "2022-12-23T07:42:02.383Z",
-    "2023-01-28T09:15:04.904Z",
-    "2023-04-01T10:17:24.185Z",
-    "2023-05-11T14:11:59.604Z",
-    "2023-05-12T17:01:17.194Z",
-    "2023-05-13T20:36:17.929Z",
-    "2023-05-14T10:51:36.790Z",
-  ],
-  currency: "DKK",
-  locale: "da-DK", // de-DE
-};
-
-const account3 = {
-  owner: "James Jonuzi",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Amina Jonuzi",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const account5 = {
-  owner: "Jonas Schmedtmann",
-  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
-  interestRate: 1.2, // %
-  pin: 5555,
-
-  movementsDates: [
-    "2019-11-18T21:31:17.178Z",
-    "2019-12-23T07:42:02.383Z",
-    "2020-01-28T09:15:04.904Z",
-    "2020-04-01T10:17:24.185Z",
-    "2020-05-08T14:11:59.604Z",
-    "2020-07-26T17:01:17.194Z",
-    "2020-07-28T23:36:17.929Z",
-    "2020-08-01T10:51:36.790Z",
-  ],
-  currency: "EUR",
-  locale: "pt-PT", // de-DE
-};
-
-const account6 = {
-  owner: "Jessica Davis",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 6666,
-
-  movementsDates: [
-    "2022-11-01T13:15:33.035Z",
-    "2022-11-30T09:48:16.867Z",
-    "2022-12-25T06:04:23.907Z",
-    "2023-05-01T14:18:46.235Z",
-    "2023-05-04T16:33:06.386Z",
-    "2023-05-05T14:43:26.374Z",
-    "2023-05-05T18:49:59.371Z",
-    "2023-05-06T12:01:20.894Z",
-  ],
-  currency: "USD",
-  locale: "en-US",
-};
-
-const accounts = [account1, account2, account3, account4, account5, account6];
+console.log(account1);
 
 const labelWelcome = document.querySelector(".nav__welcome");
 const labelDate = document.querySelector(".balance__info--date--span");
@@ -197,8 +94,8 @@ const displayMovements = function (acc, sort = false) {
             <div class="movements__row__type  movements__row--${type}">${
       index + 1
     } ${type}</div>
-            <div class="movements__date">${displayDate}</div>
-            <div class="movements__value">${displayCurrency}</div>
+            <div class="movements__row--date">${displayDate}</div>
+            <div class="movements__row--value">${displayCurrency}</div>
           </div>
       `;
 
